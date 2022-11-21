0cb6c8580980ce3d2b25001efdbe916d9d264d99
alejandrorey96 <alexrey2a@gmail.com>
2022-11-21 16:59:51 +0100

    Client test code added. Internet permissions for Manifest added.

--- app/src/main/java/com/example/metocaapk/MainActivity.java
+++ app/src/main/java/com/example/metocaapk/MainActivity.java
@@ -1,14 +1,56 @@
 package com.example.metocaapk;
 
+import android.os.StrictMode;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.example.metocaapk.R;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.net.Socket;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView pantalla;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        pantalla = (TextView) findViewById(R.id.Texto01);
+        StrictMode.setThreadPolicy(new StrictMode.ThreadPolicy.Builder()
+                .permitNetwork().build());
+        ejecutaCliente();
+    }
+
+    private void ejecutaCliente(){
+        String ip = "172.217.162.4";
+        int puerto = 7;
+        log(" socket " + ip + " " + puerto);
+        try{
+            Socket sk = new Socket(ip,puerto);
+            BufferedReader entrada = new BufferedReader(new
+                    InputStreamReader(sk.getInputStream()));
+            PrintWriter salida = new PrintWriter(
+                    new OutputStreamWriter(sk.getOutputStream()),true);
+            log("enviando ... Hola Mundo!");
+            salida.println("Hola mundo");
+            log("recibiendo ... " + entrada.readLine());
+            sk.close();
+        }
+        catch (Exception e){
+            log("error: " + e.toString());
+        }
+    }
+
+    private void log(String cadena){
+        pantalla.append(cadena + "\n");
     }
 }
